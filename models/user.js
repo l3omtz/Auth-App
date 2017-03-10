1b08baeaa428584974da80dc5f2156404b63454f
Leo Martinez <l3o@Leos-MacBook-Pro.local>
2017-03-09 22:25:57 -0600

    Register with hash passowrd

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const congif = reqire('../models/database');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
 
 // Create user Schema
 const UserSchema = mongoose.Schema({
@@ -9,26 +9,42 @@ const UserSchema = mongoose.Schema({
   },
   email: {
     type: String,
-    required: true 
+    required: true
   },
   username:{
     type: String,
     required: true
   },
-  password {
+  password: {
     type: String,
     required: true
   }
 });
 
 // Create global var for user -- export model with user of type UserSchema
-const User = module.exports.mongoose.model('User', UserSchema);
+const User = module.exports = mongoose.model('User', UserSchema);
 
 // Create functions to call outside this file
 module.exports.getUserById = function(id, callback){ // <- Takes in id and callback
   User.findById(id, callback); // <- Referse to the User we created & calls the findById mongoose function
 }
+// Get user by usernmae function
 module.exports.getUserByUsername = function(username, callback){ // <- Takes in username and callback
   const query = {username:username} // <- Query username of username
   User.findOne(query, callback); // <- Calls the findOne function which takes in a query
+}
+
+// Add user function -- export
+module.exports.addUser = function(newUser, callback){
+  // Hash the password -- generate salt
+  bcrypt.genSalt(10, (err, salt) => {
+    // Call in the password from new user
+    bcrypt.hash(newUser.password, salt, (err, hash) =>{
+      if(err) throw err;
+      // Make w/e password gave in form and make it to hash
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+
 }
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,30 @@
-// Bring in express
 const express = require('express');
-
-// Brinf in the router
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+// Bring in user model
+const User = require('../models/user');
 
 // Register the REGISTER route
 router.post('/register', (req, res, next) => {
-  res.send('REGISTER USER')
+
+  // Create newUser object from the User model
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if (err){
+      res.json({success: false, msg: "Failed to rergister user"});
+    }else {
+      res.json({success: true, msg: "User registed success"});
+    }
+  });
+
 });
 
 // Register the AUTHENTICATE route
