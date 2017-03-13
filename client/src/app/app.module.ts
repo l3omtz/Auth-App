d657e91836980b7e897eee813f8bbf8a2e754954
Leo Martinez <l3o@Leos-MacBook-Pro.local>
2017-03-12 19:20:38 -0500

    create angular components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,17 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Import components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    HomeComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
